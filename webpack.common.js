48b8700eb4a2c775fc9306ada1dff5925aecc070
Jiapan_Yu <2541771811@qq.com>
2020-07-21 11:11:36 +0800

    add prod and dev configs, dev-server
    - break our webpack.config file into 3 files
    - webpack.common.js, webpack.dev.js, and webpack.prod.js
    - install webpack-merge to share the common functionality
    - update our package.json to use the new config files
    - install webpack-dev-server and add it to start script in package.json

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,12 @@ const path = require("path")
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  // devtool: "none",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/template.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    })
+  ],
   module: {
     rules: [
       {
