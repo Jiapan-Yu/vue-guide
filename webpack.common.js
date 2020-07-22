214f0b78207d593e2f435f4f61fd8d9b0a273cba
Jiapan_Yu <2541771811@qq.com>
2020-07-22 18:01:11 +0800

    add html-loader, file-loader, and clean-webpack-plugin
    - add html-loader to automatically require the files we reference in img tags
    - add file-loader to handle the svg,png,jpg,gif files from our images
    - configure file-loader to add our images to an imgs directory in dist
    - also configure it to add a hash to their filenames
    - lastly, add clean-webpack-plugin to our production config to clean out the dist directory each time we build

--- webpack.common.js
+++ webpack.common.js
@@ -17,6 +17,21 @@ module.exports = {
           'css-loader', // 2. Turns css into commonjs
           'sass-loader', // 1. Turns sass into css
         ], // actually execute css-loader first, then style-loader
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/i,
+        use: {
+            loader: 'file-loader',
+            options: {
+              // esModule: false,
+              name: '[name].[hash].[ext]',
+              outputPath: 'imgs'
+            }
+          }
       }
     ]
   },
