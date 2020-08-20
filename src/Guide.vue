93ac37be56873ba151ad463b3d4591cb298ead86
Jiapan_Yu <2541771811@qq.com>
2020-08-20 16:24:19 +0800

    vue-router 建立，和学习官方 vuejs 教程

---
+++ src/Guide.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="vuejs-guide">
+    <a href="https://cn.vuejs.org/v2/guide/index.html">{{intro}}</a>
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      intro: "介绍"
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.vuejs-guide {
+  color: red;
+}
+</style>
