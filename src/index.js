a7ba94c16fa3f313dd58764994e04dfcdf4011f5
Jiapan_Yu <2541771811@qq.com>
2020-08-24 10:28:48 +0800

    官方 vuejs 教程 -- 计算属性和侦听器

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import Guide from './Guide.vue'
+import axios from 'axios'
+import _ from 'lodash'
 import './assets/styles/global.styl'
 
 Vue.use(VueRouter)
@@ -35,6 +37,33 @@ const router = new VueRouter({
 var app = new Vue({
   el: '#app',
   data: {
-    msg: '原始值',
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!',
+  },
+  watch: {
+    question(newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    },
+  },
+  created() {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      let vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(response => {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(error => {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    },
   },
 })
