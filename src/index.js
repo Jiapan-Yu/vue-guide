29613915c8dd9dbcda3a10ce93026d0447528f11
Jiapan_Yu <2541771811@qq.com>
2020-08-27 09:35:33 +0800

    删除 imooc 上的 todo-list 相关的代码

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import Guide from './Guide.vue'
-import './assets/styles/global.styl'
-
-Vue.use(VueRouter)
-
-const root = document.createElement('div')
-document.body.appendChild(root)
-
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
-const routes = [
-  { path: '/', component: Guide },
-  { path: '/app', component: App },
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar },
-]
-
-const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
-})
-
-
 
 Vue.component('todo-item', {
   template: `\
