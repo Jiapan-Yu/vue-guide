1edf48de3b8cf93b73c904f9c15d8ab80e15c36b
Jiapan_Yu <2541771811@qq.com>
2020-08-31 16:57:43 +0800

    作用域插槽 -- 采用默认插槽的缩写语法

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <Test>
-    <!-- 我们可以使用带值的 v-slot 来定义我们提供的插槽 prop 的名字 -->
-    <!-- 这儿我们选择将包含所有插槽 prop 的对象命名为 slotProps，但 -->
-    <!-- 你也可以使用任意你喜欢的名字 -->
-    <template v-slot:default="slotProps">
-      {{ slotProps.userName.firstName }}
-    </template>
+  <Test v-slot="slotProps">
+    {{ slotProps.userName.firstName }}
   </Test>
 </template>
 
