550b28ca14cd024dcdd4fb1c8bb0929285ea69c5
Jiapan_Yu <2541771811@qq.com>
2020-09-02 09:48:52 +0800

    官方 vuejs 教程 -- 可复用性 & 组合 -- 混入 -- 选项合并

--- src/index.js
+++ src/index.js
@@ -6,25 +6,24 @@ document.body.appendChild(root)
 
 // 定义一个混入对象
 var myMixin = {
-  created: function () {
-    this.hello()
+  data: function() {
+    return {
+      message: 'message in mixin',
+      foo: 'abc',
+    };
   },
-  methods: {
-    hello: function () {
-      console.log('hello from mixin!')
-    }
-  }
 }
-
-// 定义一个使用混入对象的组件
-var Component = Vue.extend({
-  mixins: [myMixin]
-})
-
-var component = new Component()
 
 new Vue({
   render(h) {
     return h(App)
   },
+  data: {
+    message: 'message in component',
+    bar: 'qwer',
+  },
+  created: function() {
+    console.log(this.$data);
+  },
+  mixins: [myMixin],
 }).$mount(root)
